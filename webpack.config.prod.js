d6847b8d35351047a418f9c40fd1807cbd4f69a1
Håkon Krogh <hakonhk@gmail.com>
2016-10-06 20:44:14 +0200

    Fixed css module names for production

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -39,8 +39,8 @@ module.exports = {
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        //loader: ExtractTextPlugin.extract('style-loader', 'css-loader?localIdentName=[hash:base64]&modules&importLoaders=1!postcss-loader'),
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?localIdentName=[name]&modules&importLoaders=1!postcss-loader'),
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?localIdentName=[hash:base64]&modules&importLoaders=1!postcss-loader'),
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?localIdentName=[name]__[local]&modules&importLoaders=1!postcss-loader'),
       }, {
         test: /\.css$/,
         include: /node_modules/,
