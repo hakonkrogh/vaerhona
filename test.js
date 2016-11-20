78fa4490dc2b5f9233bfe018128a9d2c81c2b892
Håkon Krogh <hakonhk@gmail.com>
2016-11-20 21:18:08 +0100

    Adde test scripts

---
+++ test.js
@@ -0,0 +1,15 @@
+// Babel polyfill to convert ES6 code in runtime
+require('babel-register')({
+  "plugins": [
+    [
+      "babel-plugin-webpack-loaders",
+      {
+        "config": "./webpack.config.babel.js",
+        "verbose": false
+      }
+    ]
+  ]
+});
+require('babel-polyfill');
+
+require('./test-run.js');
