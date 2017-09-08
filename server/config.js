47c2391288072cac0183073ec4e17371fdfd0ae9
Håkon Krogh <hakonhk@gmail.com>
2017-09-08 07:56:27 +0200

    Added GTM

--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
 const config = {
   apiUri: process.env.API_URI,
-  port: process.env.APP_PORT || 3000
+  port: process.env.APP_PORT || 3000,
+  environment: process.env.NODE_ENV || 'development'
 };
 
 module.exports = Object.freeze(config);
