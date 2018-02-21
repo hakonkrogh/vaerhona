d47ea6353966fd9151abd39bebf7690463e86747
Håkon Krogh <hakonhk@gmail.com>
2018-02-21 21:14:42 +0100

    Updated API for images and cache

--- isomorphic/api.js
+++ isomorphic/api.js
@@ -1,60 +1,69 @@
-const fetch = require('isomorphic-fetch');
+const fetch = require("isomorphic-fetch");
 
 const api = {};
 let clientInfo = {};
 
 // A generic fetch json request handler
-const jsonResponseHandler = (fetchRequest) => {
-    return fetchRequest
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response);
-        });
-}
+const jsonResponseHandler = fetchRequest => {
+  return fetchRequest.then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(response);
+  });
+};
 
 // Determine the api path
 let apiUri;
-if (typeof window !== 'undefined') {
-    apiUri = location.origin + '/api';
-} else if (typeof process !== 'undefined' && process.env && process.env.APP_PORT) {
-    apiUri = 'http://localhost:' + process.env.APP_PORT + '/api';
+if (typeof window !== "undefined") {
+  apiUri = location.origin + "/api";
+} else if (
+  typeof process !== "undefined" &&
+  process.env &&
+  process.env.APP_PORT
+) {
+  apiUri = "http://localhost:" + process.env.APP_PORT + "/api";
 }
 
 // Get the component and query from a url
-api.getRouteComponentAndMetadata = (url = '') => {
-    const apiRoute = apiUri + '/util/componentandmetadatafromroute?url=' + encodeURIComponent(url);
-    return jsonResponseHandler(fetch(apiRoute));
+api.getRouteComponentAndMetadata = (url = "") => {
+  const apiRoute =
+    apiUri +
+    "/util/componentandmetadatafromroute?url=" +
+    encodeURIComponent(url);
+  return jsonResponseHandler(fetch(apiRoute));
 };
 
 // Get the frontpage data
-api.getFrontpage = () => jsonResponseHandler(fetch(apiUri + '/frontpage'));
+api.getFrontpage = () => jsonResponseHandler(fetch(apiUri + "/frontpage"));
 
 // Get a place from name
-api.getPlace = placeName => jsonResponseHandler(fetch(apiUri + '/place/' + placeName));
+api.getPlace = placeName =>
+  jsonResponseHandler(fetch(apiUri + "/place/" + placeName));
 
 // Get snapshots for place
-api.getSnapshots = placeName => jsonResponseHandler(fetch(apiUri + '/snapshots/' + placeName));
+api.getSnapshots = placeName =>
+  jsonResponseHandler(fetch(apiUri + "/snapshots/" + placeName));
 
 // Get snapshots and place
-api.getSnapshotsAndPlace = placeName => jsonResponseHandler(fetch(apiUri + '/place-and-snapshots/' + placeName));
+api.getSnapshotsAndPlace = placeName =>
+  jsonResponseHandler(fetch(apiUri + "/place-and-snapshots/" + placeName));
 
 // Get the path to a snapshot image
 api.getImagePath = ({ snapshot }) => {
-    if (clientInfo.webp) {
-        return snapshot.imagePath;
-    }
-    if (typeof __NEXT_DATA__ !== 'undefined' && __NEXT_DATA__.props.initialState.app.clientInfo.webp) {
-        return snapshot.imagePath;
-    }
-    // if (absolutePath) {
-    //   return `${apiUri}/snapshot/${snapshot.cuid}/image`;
-    // }
-    return `/api/snapshot/${snapshot.cuid}/image`;
+  // if (clientInfo.webp) {
+  //     return snapshot.imagePath;
+  // }
+  // if (typeof __NEXT_DATA__ !== 'undefined' && __NEXT_DATA__.props.initialState.app.clientInfo.webp) {
+  //     return snapshot.imagePath;
+  // }
+  // if (absolutePath) {
+  //   return `${apiUri}/snapshot/${snapshot.cuid}/image`;
+  // }
+  return `/api/snapshot/${snapshot.cuid}/image`;
 };
 
 // Get the path to a snapshot image
-api.setClientInfo = c => clientInfo = c;
+api.setClientInfo = c => (clientInfo = c);
 
 module.exports = api;
--- server/services/place-service.js
+++ server/services/place-service.js
@@ -1,51 +1,61 @@
-const ora = require('ora');
-const config = require('../config');
+const ora = require("ora");
+const config = require("../config");
 
 const placeCache = [];
 
-async function getPlace ({ placeName, placeCuid }) {
+async function getPlace({ placeName, placeCuid }) {
+  const cachedPlace = placeCache.find(
+    p => (placeName ? p.name === placeName : p.cuid === placeCuid)
+  );
+  if (cachedPlace) {
+    return cachedPlace;
+  }
 
-    const cachedPlace = placeCache.find(p => placeName ? p.name === placeName : p.cuid === placeCuid);
-    if (cachedPlace) {
-        return cachedPlace;
+  let place;
+  try {
+    const response = await fetch(`${config.apiUri}/place/${placeName}`);
+    place = await response.json();
+
+    if (place) {
+      placeCache.push(place);
     }
+  } catch (e) {
+    console.error(e);
+  }
 
-    let place;
-    try {
-        const response = await fetch(`${config.apiUri}/place/${placeName}`);
-        place = await response.json();
-        
-        if (place) {
-            placeCache.push(place);
-        }
-    } catch (e) {
-        console.error(e);
-    }
-
-    return place;
+  return place;
 }
 
-async function getPlaces () {
-    return placeCache;
+async function getPlaces() {
+  return placeCache;
 }
 
-async function populateInitialCache () {
-    const spinner = ora('Populating initial cache for places');
-    try {
-        const response = await fetch(`${config.apiUri}/place`);
-        const places = await response.json();
-        placeCache.length = 0;
-        places.forEach(p => placeCache.push(p));
-        spinner.succeed();
-    } catch (error) {
-        spinner.fail(error.message || 'Failed to fetch places');
-        console.error(error);
-    }
-    return placeCache;
+async function rebuildCache() {
+  try {
+    const spinner = ora("Populating cache for place");
+    const response = await fetch(`${config.apiUri}/place`);
+    const places = await response.json();
+    placeCache.length = 0;
+    places.forEach(p => placeCache.push(p));
+    spinner.succeed();
+  } catch (error) {
+    spinner.fail(error);
+  }
 }
+
+async function populateInitialCache() {
+  try {
+    await rebuildCache();
+  } catch (error) {
+    console.error(error);
+  }
+  return placeCache;
+}
+
+setInterval(rebuildCache, 60 * 1000);
 
 module.exports = {
-    getPlace,
-    getPlaces,
-    populateInitialCache
+  getPlace,
+  getPlaces,
+  populateInitialCache
 };
