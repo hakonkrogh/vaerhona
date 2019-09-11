335edb81e122ece09a1e297560913cc1c5c85a99
Håkon Krogh <hakonhk@gmail.com>
2019-09-11 21:15:34 +0200

    Removed unneeded prefetch attr

--- modules/place/place-list/list-item.js
+++ modules/place/place-list/list-item.js
@@ -19,7 +19,7 @@ export default class PlaceListItem extends React.Component {
 
     return (
       <ListItem>
-        <Link as={`/${name}`} href={`/place?placeName=${name}`} prefetch>
+        <Link as={`/${name}`} href={`/place?placeName=${name}`}>
           <a>
             <PlaceName>{upperFirst(name)}</PlaceName>
             <Time>{timeOrDate(snapshot.date)}</Time>
