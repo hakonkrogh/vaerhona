d15a3d6a28d76a46455ad2b93ca24a0e5f0df44f
Håkon Krogh <hakonhk@gmail.com>
2020-09-23 20:15:27 +0200

    Use MongoDB Atlas

--- services/config.js
+++ services/config.js
@@ -5,27 +5,27 @@ if (!process.env.MONGO_DB_URI) {
 }
 
 const config = {
-  environment: process.env.NODE_ENV || 'development',
+  environment: 'production', // process.env.NODE_ENV || 'development',
 
   mongo: {
-    url: process.env.MONGO_DB_URI
+    url: process.env.MONGO_DB_URI,
   },
   aws: {
     s3BucketName: '',
-    region: ''
-  }
+    region: '',
+  },
 };
 
 const resourceNames = {
   development: 'vaerhona-development',
   staging: 'vaerhona-staging',
-  production: 'vaerhona'
+  production: 'vaerhona',
 };
 
-config.mongo.url = config.mongo.url.replace(
-  '<DATABASE>',
-  resourceNames[config.environment]
-);
+// config.mongo.url = config.mongo.url.replace(
+//   '<DATABASE>',
+//   resourceNames[config.environment]
+// );
 
 config.aws.s3BucketName = resourceNames[config.environment];
 config.aws.region = process.env.VH_AWS_REGION;
