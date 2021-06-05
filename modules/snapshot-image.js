4d0e614687acc77fe4d9ac749b4444756403d42f
Håkon Krogh <hakonhk@gmail.com>
2021-06-05 13:24:19 +0200

    wip

--- modules/snapshot-image.js
+++ modules/snapshot-image.js
@@ -3,19 +3,26 @@ import Image from 'next/image';
 
 import { prettyDateTime } from 'core/date';
 
+const myLoader = ({ src }) => {
+  return src;
+};
+
 const SnapshotImage = ({
   date,
   placeName,
   image,
   width = 1280,
   height = 960,
+  ...rest
 }) => {
   return (
     <Image
       alt={`Bilde fra ${placeName} tatt ${prettyDateTime(date)}`}
       src={image}
+      loader={myLoader}
       width={width}
       height={height}
+      {...rest}
     />
   );
 };
