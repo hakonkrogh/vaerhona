6db039b96a2616bbccf355cd9362ebd819a3c227
Håkon Krogh <hakonhk@gmail.com>
2021-09-09 14:59:37 +0200

    Yet more logs

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -5,12 +5,34 @@ import { ApolloServer } from 'apollo-server-micro';
 import { typeDefs } from '../../apollo/type-defs';
 import { resolvers } from '../../apollo/resolvers';
 
+const loggingPlugin = {
+  // Fires whenever a GraphQL request is received from a client.
+  requestDidStart(requestContext) {
+    const { query, variables } = requestContext.request;
+
+    if (!query.includes('query IntrospectionQuery')) {
+      console.log('Query:');
+      console.log(query);
+      console.log('Variables:');
+      console.log(JSON.stringify(variables, null, 1));
+
+      return {
+        willSendResponse(context) {
+          console.log('Response:');
+          console.log(JSON.stringify(context.response.data, null, 1));
+        },
+      };
+    }
+  },
+};
+
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
   playground: true,
   introspection: true,
   uploads: false,
+  plugins: [loggingPlugin],
 });
 
 export const config = {
